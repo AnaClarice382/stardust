5de77e374f17f56374458379b7a163d47b4e680e
AnaClarice382 <anaclaricep@gmail.com>
2018-10-21 01:52:15 -0300

    FE

--- lauchin.js
+++ lauchin.js
@@ -1,6 +1,21 @@
-var lancFuturos = {
-    
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.spacexdata.com/v3/launches/past', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
 
 
-
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(launch => {
+        launch.rocket.rocket_name = document.querySelector('[name=nomefoguete]'),
+        launch.rocket.rocket_name = document.querySelector('[name=nomefoguete]')
+    });
+  } else {
+    console.log('error');
+  }
 }
+
+
+request.send();
